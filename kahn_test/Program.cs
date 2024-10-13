a31fc2f83a5bd6faab5255a029522cb8a79ff681
hamza.elbakkouri <hamza.elbakkouri@marketingconfort.com>
2024-10-13 18:05:31 +0100

    change origin

--- kahn_test/Program.cs
+++ kahn_test/Program.cs
@@ -8,12 +8,16 @@ using TicketManagement.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddControllers();
+
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
 builder.Services.AddScoped<TicketServiceInterface, TicketServiceImplementation>();
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
@@ -21,31 +25,38 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ticket Management API", Version = "v1" });
 });
 
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAllOrigins", builder =>
+    options.AddPolicy("AllowSpecificOrigin", builder =>
     {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
+        builder.WithOrigins("http://localhost:3000")
+               .AllowAnyMethod()                    
+               .AllowAnyHeader()                    
+               .AllowCredentials();                 
     });
 });
 
 var app = builder.Build();
-app.UseCors("AllowAllOrigins");
+
+app.UseCors("AllowSpecificOrigin");
+
+
 if (app.Environment.IsDevelopment())
+
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else
-{
-    app.UseHttpsRedirection();
-}
-
 
 app.UseHttpsRedirection();
+
+
 app.UseAuthorization();
+
+
 app.MapControllers();
 
+
 app.Run();
+
